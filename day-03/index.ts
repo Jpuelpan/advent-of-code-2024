7090c74e91cae7bd0e77b214c88b4bb68e8bacfc
Juan Puelpan <juan@puelpan.com>
2024-12-04 23:34:13 -0300

    day-03: part two for day 3

--- day-03/index.ts
+++ day-03/index.ts
@@ -4,21 +4,35 @@
  * bun index.ts
  * */
 
-const re = RegExp(/mul\((?<a>[0-9]{1,3}),(?<b>[0-9]{1,3})\)/g);
+const re = RegExp(/mul\((?<a>[0-9]{1,3}),(?<b>[0-9]{1,3})\)|don't\(\)|do\(\)/g);
 
-const sample_input =
-  "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
+// const input_data =
+//   "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
+
+// const input_data =
+//   "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
 
 const input_data = await Bun.file("input.txt").text();
 
 let total = 0;
+let disabled = false;
 for (let match of input_data.matchAll(re)) {
-  if (!match.groups) continue;
+  if (match[0] === "don't()") {
+    disabled = true;
+    continue;
+  } else if (match[0] === "do()") {
+    disabled = false;
+    continue;
+  }
 
+  if (!match.groups || disabled) {
+    console.log(match[0], "\t=", "NOOP");
+    continue;
+  }
   const a = parseInt(match.groups["a"]);
   const b = parseInt(match.groups["b"]);
   const r = a * b;
-  console.log(match.values().next().value, "\t=", r);
+  console.log(match[0], "\t=", r);
 
   total += r;
 }
